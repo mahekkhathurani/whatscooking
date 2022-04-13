d52ee7e404325eeb5692fb78499a0aad2d6bdcc9
mahekkhathurani <72668413+mahekkhathurani@users.noreply.github.com>
2022-04-14 00:33:19 +0530

    Update config.py

--- whatscooking-deployment-master/config.py
+++ whatscooking-deployment-master/config.py
@@ -1,8 +1,8 @@
-RECIPES_PATH = "./input/df_recipes.csv"
-PARSED_PATH = "./input/df_parsed.csv"
+RECIPES_PATH = "whatscooking-deployment-master/input/df_recipes.csv"
+PARSED_PATH = "whatscooking-deployment-master/input/df_parsed.csv"
 
-TFIDF_ENCODING_PATH = "models/tfidf_encodings.pkl"
-TFIDF_MODEL_PATH = "models/tfidf.pkl"
+TFIDF_ENCODING_PATH = "whatscooking-deployment-master/models/tfidf_encodings.pkl"
+TFIDF_MODEL_PATH = "whatscooking-deployment-master/models/tfidf.pkl"
 
 LINUX_PATH = "/home/jack/projects/whatscooking-deployment/"
 OS_PATH = "/Users/Jack/Documents/Projects/whatscooking_heroku/"
